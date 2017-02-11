e51786371a60328f049f992da9882f250f6b139c
Satya Prakash Goyal <satya@spgoyal.in>
2017-02-12 02:03:53 +0530

    Fix iFrame render for android

--- src/Views/ItemDetailView.js
+++ src/Views/ItemDetailView.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import { View, Text, StyleSheet, ScrollView, WebView, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, WebView, Dimensions, Platform } from 'react-native';
 import { NavigationBar, Title, View as SView } from '@shoutem/ui';
 import HTML from 'react-native-fence-html'
+import cheerio from 'cheerio'
 
 class ItemDetailView extends React.Component {
+
+  renderVideo(){
+    if (Platform.OS == "android") {
+      const $ = cheerio.load(this.props.post.content.rendered)
+      const videoSrc = $('iframe').attr('src')
+      if (videoSrc!='' && videoSrc!=null && videoSrc!=undefined) {
+        return (
+          <View style={{width: Dimensions.get('window').width-30, height: 250}} >
+            <WebView
+              source={{uri: videoSrc}}
+              style={styles.webview}
+              startInLoadingState />
+          </View>
+        )
+      }
+    }
+  }
 
   render() {
 
@@ -39,7 +57,7 @@ class ItemDetailView extends React.Component {
         <ScrollView style={styles.contentView}>
           <SView styleName="md-gutter">
             <Title>{title.rendered}</Title>
-            <HTML html={content.rendered} renderers={renderers}/>
+            { Platform.OS == 'ios' ? <HTML html={content.rendered} renderers={renderers}/> : this.renderVideo() }
           </SView>
         </ScrollView>
       </View>
