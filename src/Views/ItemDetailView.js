a5504c12c340eac45ff9bda03dffc2b48b38b3e0
Asterism <asterism1985@gmail.com>
2017-03-23 18:51:52 +0300

    Added splash screen

--- src/Views/ItemDetailView.js
+++ src/Views/ItemDetailView.js
@@ -147,7 +147,6 @@ class ItemDetailView extends React.Component {
 
                 (function() {
                   var dsq = document.createElement("script"); dsq.type = "text/javascript"; dsq.async = true;
-                  dsq.src = "https://" + disqus_shortname + ".disqus.com/embed.js";
                   (document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(dsq);
                 })();
                 </script>
